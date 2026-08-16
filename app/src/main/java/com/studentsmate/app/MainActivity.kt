70019560c9c47c962f43716d116ba14b82e8b668
jubayer47-pro <prodhanjubayer47@gmail.com>
2026-08-16 12:13:08 +0600

    Update MainActivity.kt

--- app/src/main/java/com/studentsmate/app/MainActivity.kt
+++ app/src/main/java/com/studentsmate/app/MainActivity.kt
@@ -8,9 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.InterstitialAdLoadCallback
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+
+    private var interstitialAd: InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,5 +50,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(layout)
 
         adView.loadAd(AdRequest.Builder().build())
+
+        loadInterstitial()
+    }
+
+    private fun loadInterstitial() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-7009578840220882/4118277743",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    interstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    interstitialAd = null
+                }
+            }
+        )
     }
 }
